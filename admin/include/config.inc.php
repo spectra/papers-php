d29b6897c34e457792bcd78d5e93aa06b0aae02f
Antonio Soares Terceiro <terceiro@colivre.coop.br>
2006-06-20 23:31:04 +0000

    r240@sede:  terceiro | 2006-06-20 20:14:58 -0300
     stop if any dependency is missing.
     
    
    
    git-svn-id: https://svn.softwarelivre.org/svn/papers/trunk@233 422764cc-5101-0410-9a76-de043384d48e

--- admin/include/config.inc.php
+++ admin/include/config.inc.php
@@ -17,4 +17,50 @@ if (file_exists($configfile)) {
   exit;
 }
 
+# extension discovery
+$_needed_extensions = array(
+  'smarty',
+  'adodb',
+);
+$_extensions = array();
+
+# discover function
+function papers_ext_conf($path, $extname) {
+  global $_extensions;
+  if ($_extensions[$extname]) {
+    return;
+  }
+  if (is_dir($path)) {
+    ini_set('include_path', ini_get('include_path') . ":" . $path);
+    $_extensions[$extname] = 1;
+  }
+}
+
+# auto configuration of locally installed extensions
+$_application_path = preg_replace('/\/(admin|speaker|pub|reviewer)\/[^\/]*$/','', $_SERVER['SCRIPT_FILENAME']);
+papers_ext_conf($_application_path . '/ext/smarty/libs', 'smarty');
+papers_ext_conf($_application_path . '/ext/adodb', 'adodb');
+
+# auto configuration on Debian
+papers_ext_conf('/usr/share/adodb', 'adodb');
+papers_ext_conf('/usr/share/php/smarty/libs', 'smarty');
+
+
+# check if the needed extensions are available
+$_missing_extensions = array();
+foreach ($_needed_extensions as $ext) {
+  if (! $_extensions[$ext]) {
+    array_push($_missing_extensions, $ext);
+  }
+}
+if (! empty($_missing_extensions)) {
+  header("Content-Type: text/plain");
+  echo "The following extensions are missing: \n";
+  foreach ($_missing_extensions as $ext) {
+    echo "  $ext\n";
+  }
+  echo "\nInstall these extensions before running papers. See the INSTALL file.";
+  exit;
+}
+
 ?>
