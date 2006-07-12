d4b5a398d1c95f74411faf4642142a8121a0fa3b
Antonio Soares Terceiro <terceiro@colivre.coop.br>
2006-07-12 11:26:28 +0000

    r449@sede:  terceiro | 2006-07-12 08:27:22 -0300  removing dependency on makepasswd binary
    git-svn-id: https://svn.softwarelivre.org/svn/papers/trunk@310 422764cc-5101-0410-9a76-de043384d48e

--- speaker/include/persons.inc.php
+++ speaker/include/persons.inc.php
@@ -27,7 +27,13 @@ class Persons {
   }
 
   function newPassword($db, $cod) {
-    $passwd = str_replace("\n", '', `makepasswd --chars=8`);
+
+    $letters = explode(' ', 'a b c d e f g h i j k l m n o p q r s t u v x y w z A B C D E F G H I J K L M N O P Q R S T U V X Y W Z 0 1 2 3 4 5 6 7 8 9');
+
+    $passwd = '';
+    for($i = 0; $i < 8; $i++) {
+      $passwd .= $letters[ array_rand( $letters ) ];
+    }
 
     return Persons::setPassword($db, $cod, $passwd);
   }
