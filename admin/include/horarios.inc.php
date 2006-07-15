1fd00e579e753f0c003bc013fcc895b413ebdec8
Antonio Soares Terceiro <terceiro@colivre.coop.br>
2006-07-15 02:57:02 +0000

    r456@sede:  terceiro | 2006-07-14 23:58:09 -0300  sorting hours by number
    git-svn-id: https://svn.softwarelivre.org/svn/papers/trunk@312 422764cc-5101-0410-9a76-de043384d48e

--- admin/include/horarios.inc.php
+++ admin/include/horarios.inc.php
@@ -3,7 +3,7 @@
 class Horarios {
 
   function carregar($db) {
-    $rs = $db->conn->Execute("select numero, substring(inicio,1,5) as inicio, substring(final,1,5) as final from horarios");
+    $rs = $db->conn->Execute("select numero, substring(inicio,1,5) as inicio, substring(final,1,5) as final from horarios order by numero");
     return $rs->GetArray();
   }
 
